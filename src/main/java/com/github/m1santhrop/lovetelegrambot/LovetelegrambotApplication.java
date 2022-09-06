c67f9a6d16c227221c3d4e0d5710015b30804afb
dmitrytimofeev <dtimofeev@vimly.com>
2022-09-06 11:53:50 +0300

    Initial commit

---
+++ src/main/java/com/github/m1santhrop/lovetelegrambot/LovetelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.m1santhrop.lovetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovetelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovetelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/m1santhrop/lovetelegrambot/LovetelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m1santhrop.lovetelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovetelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
